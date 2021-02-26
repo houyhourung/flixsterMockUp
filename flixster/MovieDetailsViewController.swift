1feff5421d47d67e077f51195347f8887d3fe897
houyhourung <houyhourung88@gmail.com>
2021-02-26 11:50:18 -0500

    added poster backdrop, title, and synopsis

--- flixster/MovieDetailsViewController.swift
+++ flixster/MovieDetailsViewController.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
-
+import AlamofireImage
 class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backDropView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
     
     var movie: [String: Any]!
 
@@ -15,7 +23,22 @@ class MovieDetailsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie["title"])
+       
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backDropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        backDropView.af_setImage(withURL: backDropUrl!)
     }
     
 
